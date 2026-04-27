263546ba4e0f20776eb611ad05075333aa6cbbe9
Nate Harris <nwithan8@users.noreply.github.com>
2026-04-27 12:58:38 -0600

    - Fix Colorado Mesh name format

--- static/js/repeater_name_tool.js
+++ static/js/repeater_name_tool.js
@@ -258,7 +258,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     async function generateKeyPair(prefix) {
-        // Even though the ColoradoMesh Python library is capable of doing this,
+        // Even though the Colorado Mesh Python library is capable of doing this,
         // we do this client-side rather than server-side to ensure zero knowledge of user secrets
 
         if (typeof keyGenerator === 'undefined') {
